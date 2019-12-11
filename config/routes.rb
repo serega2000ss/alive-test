d80d3a413de8bbbb06e39d4e034f575524bf9a91
Sergey Korolyuk <sergey.moc@gmail.com>
2019-12-11 11:41:51 +0200

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  resources :dashboard, only: [:index] do
+    collection do
+      get :get_cors
+    end
+  end
+
+  resources :shop, only: [:index]
+end
